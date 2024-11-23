66e042110d99f9aa4c5485408bd4cb7b9cccc956
Andy Cordero <86756607+acordero4852@users.noreply.github.com>
2024-11-23 17:47:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+def fizzbuzz(n):
+  if n % 3 == 0 and n % 5 == 0:
+    print("FizzBuzz")
+  elif n % 3 == 0:
+    print("Fizz")
+  elif n % 5 == 0:
+    print("Buzz")
+  else
+    print(n)
+
+fizzbuzz(15)
